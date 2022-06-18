73750f745ffe6801fd828d07409b10b11bdd0754
civlon <alex695@hotmail.de>
2022-06-18 21:22:05 +0200

    Add basic frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { Button, Spinner } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 
 function App() {
+  const [data, setData] = useState({})
+
+  const getTestData = (data) => {
+    setData(data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="homepage">
+      <p>Results of testing: {JSON.stringify(data)}</p>
+      <GetTestDataButton getTestData={getTestData}>Get Test Data</GetTestDataButton>
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
+
+function GetTestDataButton({ children, getTestData, ...props }) {
+  const [isButtonLoading, setIsButtonLoading] = useState(false);
+
+  const handleOnClick = async () => {
+    setIsButtonLoading(true);
+    await fetch("/getTestData").then(res => res.json()).then(data => { getTestData(data) }, [])
+    setIsButtonLoading(false);
+  }
+
+  return (
+    <Button onClick={async () => await handleOnClick()} className="button" {...props}>
+      {isButtonLoading ? <Spinner animation="border" variant="primary" /> : children}
+    </Button>
+  );
+}
