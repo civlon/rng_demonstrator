add3b539fec5453f251253cab5fd08f16c34bd2e
civlon <alex695@hotmail.de>
2022-07-05 23:04:19 +0200

    Add mode and change mode button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { Button, Spinner } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
+const prngMode = {
+  changing: 'changing',
+  static: 'static',
+};
+
 function App() {
   const [data, setData] = useState({})
+  const [mode, setMode] = useState(prngMode.static)
 
   const getTestData = (data) => {
     setData(data)
   }
 
+  const toggleMode = () => {
+    setMode((mode) => (mode === prngMode.static ? prngMode.changing
+      : prngMode.static))
+  }
+
   return (
     <div class="homepage">
-      <p>Results of testing: {JSON.stringify(data)}</p>
-      <GetTestDataButton getTestData={getTestData}>Get Test Data</GetTestDataButton>
+      <p>testing result: {JSON.stringify(data)}</p>
+      <GetTestDataButton getTestData={getTestData} mode={mode}>Get Test Data</GetTestDataButton>
+      &nbsp;&nbsp;&nbsp;
+      <ChangePrngModeButton toggleMode={toggleMode} >Change Mode</ChangePrngModeButton>
+      <p>current mode: {mode}</p>
     </div>
   )
 
@@ -21,18 +35,27 @@ function App() {
 
 export default App
 
-function GetTestDataButton({ children, getTestData, ...props }) {
+function GetTestDataButton({ children, getTestData, mode, ...props }) {
   const [isButtonLoading, setIsButtonLoading] = useState(false);
 
   const handleOnClick = async () => {
     setIsButtonLoading(true);
-    await fetch("/getTestData").then(res => res.json()).then(data => { getTestData(data) }, [])
+    await fetch("/get?" + mode).then(res => res.json()).then(data => { getTestData(data) }, [])
     setIsButtonLoading(false);
   }
 
   return (
     <Button onClick={async () => await handleOnClick()} className="button" {...props}>
       {isButtonLoading ? <Spinner animation="border" variant="primary" /> : children}
+    </Button>
+  );
+}
+
+function ChangePrngModeButton({ children, toggleMode, ...props }) {
+
+  return (
+    <Button onClick={() => toggleMode()} className="button" {...props}>
+      {children}
     </Button>
   );
 }
